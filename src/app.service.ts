fcffa7242e91b05dd626549875e2cde5df00d6f3
tundenewcodes <fasasipelumi1652@gmail.com>
2026-01-19 14:40:36 +0100

    prisma set up completed logs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,31 +1,14 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { LoggerService } from "./core/logger/logger-service";
-
-import { CacheService } from "./core/cache/cache.service";
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly logger: LoggerService,
-    private readonly cacheService: CacheService,
-  ) {}
-  async getHello() {
-    this.logger.log("app-service");
-    const appName = this.configService.get<string>("APP_NAME");
-    console.log(appName);
-    await this.cacheService.set(`red`, `black`, 900);
-    const val = await this.cacheService.get(`red`);
-    console.log(val, `checks`);
-
-    // return {
-    //   data: "Hello to a new nestjs now World!",
-    //   success: true,
-    //   meta: {
-    //     pages: 10,
-    //   },
-    // };
-
-    return val;
+  constructor() {}
+  getHello() {
+    return {
+      data: "Hello to a new nestjs now World!",
+      success: true,
+      meta: {
+        pages: 10,
+      },
+    };
   }
 }
